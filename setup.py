4b87651a7696e7e341f0632f8a1d91be644160ae
Mike Burns <mgburns@bu.edu>
2014-04-28 13:03:32 -0400

    Adds setup.py for package install.

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='bu-sql-filter',
+    version='0.1.0',
+    author='Boston University',
+    author_email='webteam@bu.edu',
+    url='https://github.com/bu-ist/sql-filter/',
+    description='Utility for filtering SQL queries.',
+    long_description=open('README.md').read(),
+    packages=['sqlfilter'],
+    scripts=['scripts/sql-filter.py'],
+    license='LICENSE',
+    install_requires=['sqlparse']
+)
